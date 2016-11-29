98dac94964bd3c1b7ec3d08dca679d1a82e3c10c
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-29 13:41:22 +0300

    Created callbacks for handling XHR

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,27 @@
 /**
  * Created by victor on 11/29/2016.
  */
+document.getElementById('btn-build-schemas').addEventListener('click', onClickBuildSchemas, false);
+
+function onClickBuildSchemas() {
+
+   
+}
+
+/**
+ * Called when the http request was successful
+ * @param request_intent
+ * @param xhr
+ * @param response
+ */
+function onSuccessHttpRequest(request_intent, xhr, response) {
+
+}
+
+/**
+ * Called when the http request fails
+ * @param request_intent
+ * @param xhr
+ */
+function onFailedHttpRequest(request_intent, xhr) {
+}
