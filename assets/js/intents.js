725cfdec8c00d4ab2e7fe89ba5104c0e7adc8ea7
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-29 13:42:47 +0300

    Added intents.js and urls.js to hold app intents and urls

--- assets/js/intents.js
+++ assets/js/intents.js
@@ -1,3 +1,4 @@
 /**
  * Created by victor on 11/29/2016.
  */
+INTENT_BUILD_SCHEMAS = "intent-build-schemas";
--- assets/js/urls.js
+++ assets/js/urls.js
@@ -1,3 +1,4 @@
 /**
  * Created by victor on 11/29/2016.
  */
+BUILD_SCHEMAS_URL = "scripts/php/app/build-schemas.php";
