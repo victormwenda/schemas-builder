745371fb9580b4eeeb7ea8bede297ad6b3cc9d16
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-29 14:28:19 +0300

    Added validation rules, added method to send the http request

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,18 @@
 document.getElementById('btn-build-schemas').addEventListener('click', onClickBuildSchemas, false);
 
 function onClickBuildSchemas() {
+    var databaseHost = $('#input-database-host').val();
+    var databaseName = $('#input-database-name').val();
+    var databaseUser = $('#input-database-user').val();
+    var userPassword = $('#input-database-user').val();
 
-   
+    if (isFormValid(databaseHost, databaseName, databaseUser)) {
+        var request_params = "database-host=" + databaseHost + "&database-name=" + databaseName
+            + "&database-user=" + databaseUser + "&user-password=" + userPassword;
+
+        sendPOSTHttpRequest(BUILD_SCHEMAS_URL, request_params, INTENT_BUILD_SCHEMAS);
+    }
+
 }
 
 /**
@@ -24,4 +34,34 @@ function onSuccessHttpRequest(request_intent, xhr, response) {
  * @param xhr
  */
 function onFailedHttpRequest(request_intent, xhr) {
+}
+
+/**
+ * Checks if database connection  form input form is valid
+ * @param databaseHost
+ * @param databaseName
+ * @param databaseUser
+ */
+function isFormValid(databaseHost, databaseName, databaseUser) {
+    var formValid = true;
+
+    if (databaseHost == "") {
+        formValid = false;
+        document.getElementById('input-database-host').style.borderColor = "#FF0000";
+    } else {
+        document.getElementById('input-database-host').style.borderColor = "#BEBEBE";
+    }
+    if (databaseName == "") {
+        formValid = false;
+        document.getElementById('input-database-name').style.borderColor = "#FF0000";
+    } else {
+        document.getElementById('input-database-name').style.borderColor = "#BEBEBE";
+    }
+    if (databaseUser == "") {
+        formValid = false;
+        document.getElementById('input-database-user').style.borderColor = "#FF0000";
+    } else {
+        document.getElementById('input-database-user').style.borderColor = "#BEBEBE";
+    }
+    return formValid;
 }
